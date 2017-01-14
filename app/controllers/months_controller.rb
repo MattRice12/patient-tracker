a688173e0fbdbab42ea32b45f92d4c64476fcb45
Matt Rice <mattrice12@outlook.com>
2017-01-14 14:35:24 -0600

    created show page; added icons to lock, plus, and minus links

--- app/controllers/months_controller.rb
+++ app/controllers/months_controller.rb
@@ -3,4 +3,9 @@ class MonthsController < ApplicationController
     months = Month.where(user: current_user).includes(:days)
     render locals: {months: months}
   end
+
+  def show
+    month = Month.find(params.fetch(:id))
+    render locals: {month: month}
+  end
 end
