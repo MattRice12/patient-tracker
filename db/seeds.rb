3ab486c30be964cd89779c54be08803cd096348c
Matt Rice <mattrice12@outlook.com>
2017-01-19 13:26:14 -0600

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,29 +7,3 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!(name: "Matt", email: "matt@example.com", password: "password")
-User.create!(name: "Mooty", email: "mooty@example.com", password: "password")
-User.create!(name: "Mooty", email: "tooty@example.com", password: "password")
-User.create!(name: "Mooty", email: "footy@example.com", password: "password")
-User.create!(name: "Mooty", email: "dooty@example.com", password: "password")
-User.create!(name: "Mooty", email: "sooty@example.com", password: "password")
-User.create!(name: "Mooty", email: "maooty@example.com", password: "password")
-User.create!(name: "Mooty", email: "moloty@example.com", password: "password")
-User.create!(name: "Mooty", email: "moopty@example.com", password: "password")
-User.create!(name: "Mooty", email: "mootpy@example.com", password: "password")
-User.create!(name: "Mooty", email: "mootyp@example.com", password: "password")
-User.create!(name: "Mooty", email: "amooty@example.com", password: "password")
-User.create!(name: "Mooty", email: "abmooty@example.com", password: "password")
-User.create!(name: "Mooty", email: "acmooty@example.com", password: "password")
-User.create!(name: "Mooty", email: "admooty@example.com", password: "password")
-User.create!(name: "Mooty", email: "aemooty@example.com", password: "password")
-User.create!(name: "Mooty", email: "afmooty@example.com", password: "password")
-User.create!(name: "Mooty", email: "agmooty@example.com", password: "password")
-User.create!(name: "Mooty", email: "ahmooty@example.com", password: "password")
-
-1000.times do
-  Insured.create!(day_id: rand(1..365))
-end
-
-300.times do
-  Uninsured.create!(day_id: rand(1..365))
-end
