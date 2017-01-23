dd7eb5eb815eb781e431222781481a7aa0e94996
Matt Rice <mattrice12@outlook.com>
2017-01-22 22:18:35 -0600

    added styling--changed colors (still temporary), updated fonts, added _variables.scss page

--- app/models/day.rb
+++ app/models/day.rb
@@ -3,6 +3,6 @@ class Day < ApplicationRecord
 
   belongs_to :month
 
-  # validates :insureds, numericality: { greater_than_or_equal_to: 0 }
-  # validates :uninsureds, numericality: { greater_than_or_equal_to: 0 }
+  validates :insureds, numericality: { greater_than_or_equal_to: 0 }
+  validates :uninsureds, numericality: { greater_than_or_equal_to: 0 }
 end
